dea3acd1c2d11acf58526a89890ea92ca43c57fc
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-11-03 22:10:20 +0200

    fix: Use HTMLWebpackPlugin and dynamic script names to get rid of ERR_CONTENT_LENGTH_MISMATCH

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,10 +1,30 @@
 const webpack = require('webpack');
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SplitByPathPlugin = require('webpack-split-by-path');
 
 const { isDevelopment, isProduction } = require('./env');
 
 const plugins = [
-    new webpack.EnvironmentPlugin(['NODE_ENV'])
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new HtmlWebpackPlugin({
+        template: 'index.html',
+        chunksSortMode: (a, b) => {
+            const order = ['manifest', 'vendor', 'app'];
+            const nameA = a.names[0];
+            const nameB = b.names[0];
+
+            return order.indexOf(nameA) - order.indexOf(nameB);
+        }
+    }),
+    new SplitByPathPlugin([{
+        name: 'vendor',
+        path: path.join(__dirname, '..', 'packages/frontend/node_modules/')
+    }], {
+        // fix https://github.com/webpack/extract-text-webpack-plugin/issues/92
+        ignore: [/\.css/]
+    })
 ];
 
 if (isDevelopment) {
